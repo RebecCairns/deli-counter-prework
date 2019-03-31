aef430b8e7d60e34809d06aa955c8b527df2279c
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-03-31 23:12:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,13 @@ def line(arr)
      puts "The line is currently empty."
     end
     if(arr.length > 0)
-      
+      returner = "The line is currently:"
+      counter = 1 
       arr.each do
-        
-        
+        returner += " #{counter}. #{arr[counter-1]}" 
+        counter += 1
       end
+      returner += "."
+      puts returner
     end  
 end   
