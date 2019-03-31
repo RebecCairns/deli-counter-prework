eed46ebd0fc3e82e075a1daf95a7f1dc91f5ee86
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-03-31 23:08:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,4 +3,11 @@ def line(arr)
     if(arr.length == 0)
      puts "The line is currently empty."
     end
+    if(arr.length > 0)
+      
+      arr.each do
+        
+        
+      end
+    end  
 end   
